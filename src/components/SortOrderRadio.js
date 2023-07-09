62535a7ad5e2eed65e5ddb6a63dc250f3adc9ebd
Sarah Phillips <snphillips@gmail.com>
2023-07-09 18:59:38 -0400

    rename sort buttons to be more user friendly

--- src/components/SortOrderRadio.js
+++ src/components/SortOrderRadio.js
@@ -1,51 +1,42 @@
 import React from 'react';
 
-
 export default function SortOrder(props) {
-       return (
+  return (
+    <form className='radio-toolbar' id='radio-toolbar-sort' onChange={props.sortOrderRadioSubmit}>
+      <input
+        type='radio'
+        className='radio-sort'
+        id='sort-ascending'
+        name='radioSort'
+        value='sort ascending'
+        onChange={props.sortOrderRadioSubmit}
+        defaultChecked
+      />
 
-              <form
-                     className="radio-toolbar"
-                     id="radio-toolbar-sort"
-                     onChange={props.sortOrderRadioSubmit}
-              >
+      <label htmlFor='sort-ascending'>sort least to most</label>
+      <br />
 
-                     <input
-                            type="radio"
-                            className="radio-sort"
-                            id="sort-ascending"
-                            name="radioSort"
-                            value="sort ascending"
-                            onChange={props.sortOrderRadioSubmit}
-                            defaultChecked
-                     />
+      <input
+        type='radio'
+        className='radio-sort'
+        id='sort-descending'
+        name='radioSort'
+        value='sort descending'
+        onChange={props.sortOrderRadioSubmit}
+      />
+      <label htmlFor='sort-descending'>sort most to least</label>
+      <br />
 
-                     <label htmlFor="sort-ascending">sort ascending</label>
-                     <br />
-
-                     <input
-                            type="radio"
-                            className="radio-sort"
-                            id="sort-descending"
-                            name="radioSort"
-                            value="sort descending"
-                            onChange={props.sortOrderRadioSubmit}
-                     />
-                     <label htmlFor="sort-descending">sort descending</label>
-                     <br />
-
-                     <input
-                            type="radio"
-                            className="radio-sort"
-                            id="sort-alphabetical"
-                            name="radioSort"
-                            value="sort alphabetical"
-                            onChange={props.sortOrderRadioSubmit}
-                     />
-                     <label htmlFor="sort-alphabetical">sort alphabetical</label>
-                     <br />
-
-              </form>
-
-       );
+      <input
+        type='radio'
+        className='radio-sort'
+        id='sort-alphabetical'
+        name='radioSort'
+        value='sort alphabetical'
+        onChange={props.sortOrderRadioSubmit}
+      />
+      <label htmlFor='sort-alphabetical'>sort alphabetical</label>
+      <br />
+    </form>
+  );
 }
