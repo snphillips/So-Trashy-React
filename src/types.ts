6e6bf975fc4a7571102ba18410003f7718d834d0
Sarah Phillips <snphillips@gmail.com>
2023-08-17 18:07:38 -0400

    add some custom types to types file

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export type RefuseHeadingType = (
+  'all trash/recycling/compost' |
+  'trash' |
+  'paper & cardboard' |
+  'metal/glass/plastic' |
+  'brown bin organics' |
+  'leaves' |
+  'christmas trees'
+)
+
+export type RefuseType = (
+  'allcollected'|
+  'refusetonscollected'|
+  'papertonscollected'|
+  'mgptonscollected'|
+  'resorganicstons'|
+  'leavesorganictons'|
+  'xmastreetons'
+)
+
+export type BoroughType = (
+  'Bronx' |
+  'Brooklyn' |
+  'Manhattan' |
+  'Queens' |
+  'Staten Island' 
+)
