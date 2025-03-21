91a31d0320c14d6a733e81096180eab30d718d1f
Sarah Phillips <snphillips@gmail.com>
2025-03-21 15:18:24 -0400

    refactor year button

--- src/components/YearButton.tsx
+++ src/components/YearButton.tsx
@@ -5,67 +5,24 @@ type Props = {
   year: number,
 };
 
+const currentYear = new Date().getFullYear();
+const years = Array.from({ length: currentYear - 2010 + 1 }, (_, index) => currentYear - index);
+
 export default function YearButton({
   yearDropdownSubmit,
   year
 }: Props) {
   return (
     <form onChange={yearDropdownSubmit}>
-      <select 
-      className='dropdown-button'
-      defaultValue={year}
-      >
-        <option className='year' value={new Date().getFullYear()} id='default-current-year'>
+      <select className="dropdown-button" defaultValue={year}>
+        <option className="year" value={currentYear} id="default-current-year">
           year
         </option>
-        <option className='year' value={2025}>
-          2025
-        </option>
-        <option className='year' value={2024}>
-          2024
-        </option>
-        <option className='year' value={2023}>
-          2023
-        </option>
-        <option className='year' value={2022}>
-          2022
-        </option>
-        <option className='year' value={2021}>
-          2021
-        </option>
-        <option className='year' value={2020}>
-          2020
-        </option>
-        <option className='year' value={2019}>
-          2019
-        </option>
-        <option className='year' value={2018}>
-          2018
-        </option>
-        <option className='year' value={2017}>
-          2017
-        </option>
-        <option className='year' value={2016}>
-          2016
-        </option>
-        <option className='year' value={2015}>
-          2015
-        </option>
-        <option className='year' value={2014}>
-          2014
-        </option>
-        <option className='year' value={2013}>
-          2013
-        </option>
-        <option className='year' value={2012}>
-          2012
-        </option>
-        <option className='year' value={2011}>
-          2011
-        </option>
-        <option className='year' value={2010}>
-          2010
-        </option>
+        {years.map(year => (
+          <option key={year} className="year" value={year}>
+            {year}
+          </option>
+        ))}
       </select>
     </form>
   );
